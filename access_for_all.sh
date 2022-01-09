ad13f9a7f6d64f743029e5f78e151222f015fe46
Dragan Cajic <cajic_dragan@yahoo.com>
2022-01-09 19:18:05 +0100

    $ Working with file modes and permissions

---
+++ access_for_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+solve() {
+	# add your solution here
+	chmod 777 /tmp/test.exe
+}
+
+# uncomment before running the script!
+#solve
---
+++ other_permissions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+solve() {
+	# add your solution here
+	chmod o+x /tmp/program.py
+}
+
+# uncomment before running the script!
+#solve
