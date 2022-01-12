d38adda4bd41fce6879bfe5f4d1a3275f267bac9
Dragan Cajic <cajic_dragan@yahoo.com>
2022-01-12 05:47:10 +0100

    $ Copy and delete files

---
+++ copy_directory.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+function solve() {
+	# add your solution here
+	cp -r apps programs
+}
+
+# uncomment before running the script!
+#solve
---
+++ move_file.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+function solve() {
+	# add your solution here
+	mv secret.txt ./secrets
+}
+
+# uncomment before running the script!
+#solve
