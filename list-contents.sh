63c253a768ace5dd8059e280f6243a70b01db008
Dragan Cajic <cajic_dragan@yahoo.com>
2023-01-17 16:28:00 +0100

    $ Show contents of current directory with one command > Computer science → Fundamentals → Dev tools → Command line → Basic principles > § Command line overview > ~ Tough interview question > https://hyperskill.org/learn/step/19906

---
+++ list-contents.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+function solve() {
+	# add your solution using shell here
+	ls
+}
+
+# uncomment before running the script!
+#solve
+# ------------------------------------
+# to run script in terminal type:
+# $ bash ./list-contents.sh
